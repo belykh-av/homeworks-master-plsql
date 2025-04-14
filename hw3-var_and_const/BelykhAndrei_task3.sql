16f64ae13b17c99d2ebdadfd39b457a22bb2f654
Белых Андрей <belykhav@mail.ru>
2025-04-14 22:50:16 +0300

    Корреткировка по замечаниям

--- hw3-var_and_const/BelykhAndrei_task3.sql
+++ hw3-var_and_const/BelykhAndrei_task3.sql
@@ -2,55 +2,49 @@
 --Описание скрипта: API для сущностей “Платеж” и “Детали платежа”
 
 declare
-  c_status constant number := 0;
-  v_couse varchar2(4000);
+  c_status_created constant number := 0;
   v_message varchar2(4000) := 'Платеж создан.';
 begin
-  v_message := v_message || ' Статус: ' || c_status;
+  v_message := v_message || ' Статус: ' || c_status_created;
   dbms_output.put_line(v_message);
 end;
 /
 
 --Сброс платежа в "ошибочный статус"
 declare
-  c_status constant number := 2;
-  v_couse varchar2(4000) := 'недостаточно средств';
+  c_status_failed constant number := 2;
+  v_cause varchar2(4000) := 'недостаточно средств';
   v_message varchar2(4000)
     := 'Сброс платежа в "ошибочный статус" с указанием причины.';
 begin
-  v_message := v_message || ' Статус: ' || c_status || '.';
-  v_message := v_message || ' Причина: ' || v_couse;
+  v_message := v_message || ' Статус: ' || c_status_failed || '. Причина: ' || v_cause;
   dbms_output.put_line(v_message);
 end;
 /
 
 --Отмена платежа
 declare
-  c_status constant number := 3;
-  v_couse varchar2(4000) := 'ошибка пользователя';
+  c_status_canceled constant number := 3;
+  v_cause varchar2(4000) := 'ошибка пользователя';
   v_message varchar2(4000) := 'Отмена платежа с указанием причины.';
 begin
-  v_message := v_message || ' Статус: ' || c_status || '.';
-  v_message := v_message || ' Причина: ' || v_couse;
+  v_message := v_message || ' Статус: ' || c_status_canceled || '. Причина: ' || v_cause;
   dbms_output.put_line(v_message);
 end;
 /
 
 --Успешное завершение платежа
 declare
-  c_status constant number := 1;
-  v_couse varchar2(4000);
+  c_status_completed constant number := 1;
   v_message varchar2(4000) := 'Успешное завершение платежа.';
 begin
-  v_message := v_message || ' Статус: ' || c_status;
+  v_message := v_message || ' Статус: ' || c_status_completed;
   dbms_output.put_line(v_message);
 end;
 /
 
 --Данные платежа добавлены или обновлены
 declare
-  c_status constant number := null;
-  v_couse varchar2(4000);
   v_message varchar2(4000)
     := 'Данные платежа добавлены или обновлены по списку id_поля/значение';
 begin
@@ -60,8 +54,6 @@ end;
 
 --Детали платежа удалены
 declare
-  c_status constant number := null;
-  v_couse varchar2(4000);
   v_message varchar2(4000) := 'Детали платежа удалены по списку id_полей';
 begin
   dbms_output.put_line(v_message);
